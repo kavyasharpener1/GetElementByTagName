af9922974734fbbdd66b9760d6a813d01119ddac
kavyasharpener1 <naikkavya201@gmail.com>
2022-12-07 22:48:09 +0530

    QuerySelector && QuerySelectorALL

--- dom.js
+++ dom.js
@@ -8,4 +8,6 @@ var listitems = document.getElementsByTagName('li');
 console.log(listitems[4].textContent)
 listitems[4].textContent='New Item';
 
-
+//QUERYSELECTOR
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'green';
